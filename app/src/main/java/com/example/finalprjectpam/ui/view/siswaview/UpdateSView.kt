fc0953f324b73b48163b535bea666ea97a41a48d
ijaamm <ijammusyaf@gmail.com>
2025-01-26 13:32:15 +0700

    membuat tampilan update siswa view

---
+++ app/src/main/java/com/example/finalprjectpam/ui/view/siswaview/UpdateSView.kt
@@ -0,0 +1,159 @@
+package com.example.finalprjectpam.ui.view.siswaview
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalprjectpam.ui.customewidget.CostumeTopAppBar
+import com.example.finalprjectpam.ui.navigasi.DestinasiNavigasi
+import com.example.finalprjectpam.ui.viewmodel.PenyediaViewModel
+import com.example.finalprjectpam.ui.viewmodel.siswaviewmodel.UpdateSViewModel
+import com.example.finalprjectpam.ui.viewmodel.siswaviewmodel.UpdateUiEvent
+import com.example.finalprjectpam.ui.viewmodel.siswaviewmodel.UpdateUiState
+import kotlinx.coroutines.launch
+
+object DestinasiUpdateS: DestinasiNavigasi {
+    override val route = "updateSiswa"
+    override val titleRes = "Update Siswa"
+    const val idSsw = "id_siswa"
+    val routeWithArgs = "$route/{$idSsw}"
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreenS(
+    idSiswa: String,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateSViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(idSiswa) {
+        viewModel.loadSiswa(idSiswa)
+    }
+
+    val updateUiState = viewModel.UpdateuiState
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdateS.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        UpdateBody(
+            updateUiState = updateUiState,
+            onValueChange = viewModel::updateState,
+            onUpdateClick = {
+                coroutineScope.launch {
+                    viewModel.updateSsw()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onValueChange: (UpdateUiEvent) -> Unit,
+    onUpdateClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(26.dp)
+    ) {
+        UpdateFormInput(
+            updateUiEvent = updateUiState.updateUiEvent,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onUpdateClick,
+            shape = MaterialTheme.shapes.small,
+            colors = ButtonDefaults.buttonColors(Color(0xFFa4c639)),
+        ) {
+            Text(text = "Update")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateFormInput(
+    updateUiEvent: UpdateUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (UpdateUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = updateUiEvent.idSiswa,
+            onValueChange = { onValueChange(updateUiEvent.copy(idSiswa = it)) },
+            label = { Text(text = "Id Siswa") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.namaSiswa,
+            onValueChange = { onValueChange(updateUiEvent.copy(namaSiswa = it)) },
+            label = { Text(text = "Nama Siswa") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.emailS,
+            onValueChange = { onValueChange(updateUiEvent.copy(emailS = it)) },
+            label = { Text(text = "Email") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.noTeleponS,
+            onValueChange = { onValueChange(updateUiEvent.copy(noTeleponS = it)) },
+            label = { Text(text = "Nomor Telepon") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
+}
